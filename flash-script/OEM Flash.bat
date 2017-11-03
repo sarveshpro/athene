b141269b2ee4ce5dd5181b9278a59a3f84d0cbe3
Sarvesh Warge <sarveshwarge@gmail.com>
2017-11-04 01:45:23 +0530

    ref

---
+++ flash-script/OEM Flash.bat
@@ -0,0 +1,24 @@
+fastboot flash partition gpt.bin
+fastboot flash bootloader bootloader.img
+fastboot flash logo logo.bin
+fastboot flash boot boot.img
+fastboot flash recovery recovery.img
+fastboot flash dsp adspso.bin
+fastboot flash oem oem.img
+fastboot flash system system.img_sparsechunk.0
+fastboot flash system system.img_sparsechunk.1
+fastboot flash system system.img_sparsechunk.2
+fastboot flash system system.img_sparsechunk.3
+fastboot flash system system.img_sparsechunk.4
+fastboot flash system system.img_sparsechunk.5
+fastboot flash system system.img_sparsechunk.6
+fastboot flash system system.img_sparsechunk.7
+fastboot flash modem NON-HLOS.bin
+fastboot erase modemst1
+fastboot erase modemst2
+fastboot flash fsg fsg.mbn
+fastboot erase cache
+fastboot erase userdata
+fastboot erase customize
+fastboot erase clogo
+fastboot reboot
